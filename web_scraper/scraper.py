13ff76b7384d712f53622da7344c4830b570f175
Joe Streifel <josephstreifel@gmail.com>
2021-12-21 17:32:19 -0800

    completes citation count function

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -8,9 +8,30 @@ import requests
 # TODO: Create a function that accepts a url and returns the number of citations needed.
 # TODO: Create a function that accepts a wikipedia url and returns a string containing each entry on a new line, in the order found
 
-def get_citations_needed_count(url):
-    pass
+URL = "https://en.wikipedia.org/wiki/Monosodium_glutamate"
 
+def get_citations_needed_count(url):
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    
+    citations = soup.findAll("a", string="citation needed")
+
+    citation_num = len(citations)
+
+    print("Number of citations needed:", citation_num)
+    # for citation in citations:
+    #     print(citation.find_parent("p").text)
+
+    # print(citations.prettify())
+
+    return citation_num
+    
 
 def get_citations_needed_report(url):
     pass
+
+
+get_citations_needed_count(URL)
+
+# get_citations_needed_report(URL)
