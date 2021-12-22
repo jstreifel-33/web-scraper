2ef527bd8156d5361eea61f62e2730f0ddccedc8
Joe Streifel <josephstreifel@gmail.com>
2021-12-21 17:08:35 -0800

    adds gitignore, todo list, and initial functions

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,3 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
+# example url: https://en.wikipedia.org/wiki/Monosodium_glutamate
+
+#Tasks:
+# TODO: Create a web scraper accepts a wikipedia url. It should be composed of two functions:
+# TODO: Create a function that accepts a url and returns the number of citations needed.
+# TODO: Create a function that accepts a wikipedia url and returns a string containing each entry on a new line, in the order found
+
+def get_citations_needed_count(url):
+    pass
+
+
+def get_citations_needed_report(url):
+    pass
